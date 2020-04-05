ddfb1e064490c714f3d571c6063c1d91757e5904
Lakshay Nanda <vineetnanda@Lakshays-MacBook-Air.local>
2020-04-05 10:10:23 +0530

    Second one

---
+++ LevelUp/Recursion/floodFill.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+import javax.lang.model.util.ElementScanner14;
+public class floodFill 
+{
+    static int counter=0;
+    public static void floodfill(int sr,int sc,int mat[][],boolean[][] visited,String psf)
+    {
+        if(sr==mat.length-1 && sc==mat[0].length-1)
+        {
+            counter++;
+            System.out.print(counter+". "+psf);
+            return;
+        }
+        if(isAtWrong(sr, sc, mat)==true || visited[sr][sc]==true)
+        {
+            return;
+        }
+        visited[sr][sc]=true;
+        floodfill(sr+1, sc, mat,visited, psf+"D");
+        floodfill(sr, sc+1, mat,visited, psf+"R");
+        floodfill(sr-1, sc, mat,visited, psf+"U");
+        floodfill(sr, sc-1, mat,visited, psf+"L");
+    }
+    public static boolean isAtWrong(int sr,int sc,int mat[][])
+    {
+        if(sc>mat[0].length-1 || sr>mat.length-1)
+        {
+            return true;
+        }
+        else if(sc<0 || sr<0)
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
+    }
+    public static void main(String args[])
+    {
+        int[][] mat={{1,0,1,1},{1,1,1,1},{1,0,1,1},{1,1,0,1}};
+        boolean[][] visited =new boolean[mat.length][mat[0].length];
+        floodfill(0, 0, mat, visited, "");
+    }
+}
